0cd7b37fa171bc53c1d753e0cd2c69a7d286f45c
Erik Jälevik <erikjalevik@fastmail.fm>
2022-06-12 13:22:44 +0200

    Scripts for copying to/from vscode directory - Win

---
+++ vscode/from-win.sh
@@ -0,0 +1,2 @@
+cp /c/Users/Erik/AppData/Roaming/Code/User/settings.json ./settings.json
+cp /c/Users/Erik/AppData/Roaming/Code/User/keybindings.json ./win/keybindings.json
---
+++ vscode/to-win.sh
@@ -0,0 +1,2 @@
+cp ./settings.json /c/Users/Erik/AppData/Roaming/Code/User/settings.json
+cp ./win/keybindings.json /c/Users/Erik/AppData/Roaming/Code/User/keybindings.json
