7c6ca10c03124e9660295475918a868eff6364d9
Erik Jälevik <erikjalevik@fastmail.fm>
2022-07-25 12:07:47 +0200

    Add snippets file

--- vscode/from-mac.sh
+++ vscode/from-mac.sh
@@ -1,2 +1,3 @@
 cp ~/Library/Application\ Support/Code/User/settings.json ./settings.json
 cp ~/Library/Application\ Support/Code/User/keybindings.json ./mac/keybindings.json
+cp ~/Library/Application\ Support/Code/User/snippets/* ./snippets/
