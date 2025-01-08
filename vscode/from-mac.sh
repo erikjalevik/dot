4936910c42ec838168a7ab6eea4d59bd16ae1efc
Erik Jälevik <erikjalevik@fastmail.fm>
2025-01-08 10:10:45 +0100

    Add Cursor configs too - Mac

--- vscode/from-mac.sh
+++ vscode/from-mac.sh
@@ -1,3 +1,10 @@
-cp ~/Library/Application\ Support/Code/User/settings.json ./settings.json
-cp ~/Library/Application\ Support/Code/User/keybindings.json ./mac/keybindings.json
+set -x # echo commands to stdout
+
+cp ~/Library/Application\ Support/Code/User/settings.json ./settings.vscode.json
+cp ~/Library/Application\ Support/Code/User/keybindings.json ./mac/keybindings.vscode.json
 cp ~/Library/Application\ Support/Code/User/snippets/* ./snippets/
+
+cp ~/Library/Application\ Support/Cursor/User/settings.json ./settings.cursor.json
+cp ~/Library/Application\ Support/Cursor/User/keybindings.json ./mac/keybindings.cursor.json
+
+set +x
--- vscode/to-mac.sh
+++ vscode/to-mac.sh
@@ -1,2 +1,6 @@
-cp ./settings.json ~/Library/Application\ Support/Code/User/settings.json
-cp ./mac/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+cp ./settings.vscode.json ~/Library/Application\ Support/Code/User/settings.json
+cp ./mac/keybindings.vscode.json ~/Library/Application\ Support/Code/User/keybindings.json
+cp ./snippets/* ~/Library/Application\ Support/Code/User/snippets/
+
+cp ./settings.cursor.json ~/Library/Application\ Support/Cursor/User/settings.json
+cp ./mac/keybindings.cursor.json ~/Library/Application\ Support/Cursor/User/keybindings.json
